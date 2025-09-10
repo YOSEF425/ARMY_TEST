ce99497d143d461b358fd48adbce519aa5c254e4
Yosef Steinberg <steinbergyosef@gmail.com>
2025-09-10 10:39:11 +0300

    fixed route

--- linkodkod/backend/controllers.js
+++ linkodkod/backend/controllers.js
@@ -1,5 +1,4 @@
 import * as fs from 'fs/promises';
-import { json } from 'stream/consumers';
 
 
 
@@ -10,29 +9,43 @@ import { json } from 'stream/consumers';
 
 export async function getAllPosts(req,res){
   
-    const raw = await fs.readFile('posts.json','utf-8')
+    const raw = await fs.readFile('posts2.json','utf-8')
     const posts = JSON.parse(raw)
     res.json(posts)
   
  
 }
 
+
 export async function getPostById(req,res){
-  
-        
-    const id = req.params.id
-  
-    const raw = await fs.readFile('posts.json','utf-8')
-    const posts = JSON.parse(raw)
-    for(let post in posts){
-        if(post[id] === id){
-            res.json(post)
-        }
-        else{
-            res.send("There's no ID like that in the DB!")
-        }
-    }
+   
+      let id =""
+      id = req.params.id
+      let posts = []
+      let found = false
+      
     
+     
+       const raw = await fs.readFile('posts2.json','utf-8')
+       posts = await JSON.parse(raw)
+       
+       
+     
+        for(let index = 0; index < posts.lenght ; index ++){
+            if(posts[index][0] === id){
+                found = true
+                break
+            }
+        }
+
+        if(found){
+            res.send(posts[index])
+        }else{
+
+            res.send("Could'nt find ID!")
+        }
+
+
 }
 
 
