8495b4f3c83bcbe347e8940f60a246b2e136ee82
Yosef Steinberg <steinbergyosef@gmail.com>
2025-09-10 11:19:38 +0300

    added find post front

--- linkodkod/src/components/application-layout/findPost.tsx
+++ linkodkod/src/components/application-layout/findPost.tsx
@@ -3,28 +3,40 @@ import '../../style/findPost.css'
 import Post2 from "./post2"
 
 export default function FindPost(){
+    const[found,setFound] = useState(false)
     const [id,setId] = useState('')
     const[authorName,setAuthorName] = useState('')
     const[url,setUrl] = useState('')
     const[description,setDescription] = useState('')
 
-      
-      async function fetchPost(id:any){
-        const response = await fetch(`http://localhost:3000/posts/${id}`)
-        const data = await response.json();
-        setAuthorName(data.name)
-        setUrl(data.url)
-        setDescription(data.description)
-
-      }
-
-
-      function handleSubmit(e:any) {
+     function handleSubmit(e:any) {
         e.preventDefault();
       }
 
-      function handleChange(e:any) {
+
+      async function fetchPost(id:any){
+        try{
+          const response = await fetch(`http://localhost:3000/posts/${id}`)
+  
+          const data = await response.json();
+          console.log(data)
+          setAuthorName(data.name)
+          setUrl(data.url)
+          setDescription(data.description)
+          console.log(data)
+
+        }catch(error){
+          console.log("no")
+
+        }
+      }
+
+
+     
+      function handleChange(e:any) {    // When user clicks the "search" button the useState is set to that value
+                                       //  and we fetch the endpoint of that id.
         setId(e.target.value);
+        console.log(id)
         fetchPost(id)
       }
 
@@ -34,14 +46,14 @@ export default function FindPost(){
 
     return(
         <>
-        <form onSubmit={handleChange}>
+        
         <div className="searchBox">
         <h3>Find post by ID</h3>
         <div>Enter the ID of author in the box:</div>
-        <input type="text" className="input"/>
+        <input type="text" name="id" title="ID" placeholder="ID" className="input"/>
         </div>
-        <button type="submit">Search</button>
-        </form>
+        <button onClick={handleChange}>Search</button>
+        
         </>
     )
 }
